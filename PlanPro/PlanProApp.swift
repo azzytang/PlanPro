5bb3aeba09ea6f245bc50be92b4dc569b6eb6b7c
Azalea Tang <azzytang@gmail.com>
2024-06-24 13:40:35 -0500

    Initial Commit

---
+++ PlanPro/PlanProApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanProApp.swift
+//  PlanPro
+//
+//  Created by Azalea Tang on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PlanProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
